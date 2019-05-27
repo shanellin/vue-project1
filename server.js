40c30c2092b6ab77e64dd361f2af9c2a1f47d79d
ShaneLin <shane.sandy123@gmail.com>
2019-05-28 00:39:48 +0800

    fix router 404

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
+var history = require('connect-history-api-fallback');
 
 var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
-// const http = require('http')
-// const fs = require('fs')
-// const httpPort = 80
-//
-// http.createServer((req, res) => {
-//   fs.readFile('index.html', 'utf-8', (err, content) => {
-//     if (err) {
-//       console.log('We cannot open "index.html" file.')
-//     }
-//     res.writeHead(200, {
-//       'Content-Type': 'text/html; charset=utf-8'
-//     })
-//     res.end(content)
-//   })
-// }).listen(httpPort, () => {
-//   console.log('Server listening on: http://localhost:%s', httpPort)
-// })
+app.use(express.static('dist'));
+app.use(history({ verbose: true, index: '/' }));
 
-var port = process.env.PORT || 5000
-app.listen(port)
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('./dist/index.html'));
+});
+
+// server
+var server = app.listen(process.env.PORT || 5000, (req, res) => {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('server is running .  http://%s:%s', host, port);
+});
+
+// var port = process.env.PORT || 5000
+// app.listen(port)

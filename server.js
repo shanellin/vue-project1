a849081639593e0b3848cb15050a7644a2dddf41
ShaneLin <shane.sandy123@gmail.com>
2019-05-28 00:02:58 +0800

    fix refresh page 404

--- server.js
+++ server.js
@@ -5,23 +5,23 @@ var serveStatic = require('serve-static')
 var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
-const http = require('http')
-const fs = require('fs')
-const httpPort = 80
+// const http = require('http')
+// const fs = require('fs')
+// const httpPort = 80
+//
+// http.createServer((req, res) => {
+//   fs.readFile('index.html', 'utf-8', (err, content) => {
+//     if (err) {
+//       console.log('We cannot open "index.html" file.')
+//     }
+//     res.writeHead(200, {
+//       'Content-Type': 'text/html; charset=utf-8'
+//     })
+//     res.end(content)
+//   })
+// }).listen(httpPort, () => {
+//   console.log('Server listening on: http://localhost:%s', httpPort)
+// })
 
-http.createServer((req, res) => {
-  fs.readFile('index.html', 'utf-8', (err, content) => {
-    if (err) {
-      console.log('We cannot open "index.html" file.')
-    }
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    })
-    res.end(content)
-  })
-}).listen(httpPort, () => {
-  console.log('Server listening on: http://localhost:%s', httpPort)
-})
-
-// var port = process.env.PORT || 5000
-// app.listen(port)
+var port = process.env.PORT || 5000
+app.listen(port)
